bbd271762a641fc2579185a45eab330a77e10a7b
AllanMuhari <allanmuhari087@gmail.com>
2025-04-27 00:33:10 +0300

    .remove .env from public view

--- frontend/src/pages/Login.jsx
+++
@@ -1,7 +0,0 @@
-import LoginForm from "../components/auth/Login";
-
-const Login = () => {
-  return <LoginForm />;
-};
-
-export default Login;
--- frontend/src/pages/Register.jsx
+++
@@ -1,7 +0,0 @@
-import RegisterForm from "../components/auth/Register";
-
-const Register = () => {
-  return <RegisterForm />;
-};
-
-export default Register;
